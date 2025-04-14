b13f9e853958b2ec6463d275f6cbfa7e8455b106
Jonathan <jonathan.lichtenfeld@gmail.com>
2025-04-14 14:06:36 +0200

    Change default value in code

--- xsens_driver/mtnode.py
+++ xsens_driver/mtnode.py
@@ -28,7 +28,7 @@ class XSensDriver(rclpy.node.Node):
         baudrate = self.get_param('baudrate', 0)
         timeout = self.get_param('timeout', 0.002)
         initial_wait = self.get_param('initial_wait', 0.1)
-        self.enable_diagnostics = self.get_param('enable_diagnostics', False)
+        self.enable_diagnostics = self.get_param('enable_diagnostics', True)
         if device == 'auto':
             devs = mtdevice.find_devices(timeout=timeout, initial_wait=initial_wait)
             if devs:
